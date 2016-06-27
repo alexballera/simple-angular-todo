7060cf2da07d9b65231650f198d542440c79edc9
Alex Ballera <alexballera@gmail.com>
2016-06-26 21:12:07 -0400

    Mejorando estilos

--- src/scripts/controller.js
+++ src/scripts/controller.js
@@ -18,10 +18,6 @@ angular
     // $scope.remaining = remaining
     // $scope.archive = archive
 
-    // $scope.addTodo = function () {
-    //   if ($scope.todoText !== '') $scope.todo.push({text: $scope.todoText, done: false})
-    //   $scope.todoText = ''
-    // }
     $scope.addTodo = function () {
       $scope.todo.push({text: $scope.newActv, done: false})
       $scope.newActv = ''
@@ -37,14 +33,19 @@ angular
 
     $scope.archive = function () {
       var oldtodo = $scope.todo
-          // console.log(oldtodo)
       $scope.todo = []
       angular.forEach(oldtodo, function (todo) {
         if (!todo.done) $scope.todo.push(todo)
-        // else $scope.todo.push(oldtodo)
-        console.log(todo)
-        console.log(oldtodo)
       })
     }
+
+    $scope.showTodos = function () {
+      var oldtodo = $scope.todo
+      $scope.todo = []
+      angular.forEach(oldtodo, function (todo) {
+        if (todo.done) $scope.todo.push(todo)
+      })
+    }
+    console.log($scope.todo)
   }
 })()
